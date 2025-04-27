aa94746719d753d37d53d02dde6c75ac3cdc7636
Kilo <37485242+DevKilo1@users.noreply.github.com>
2025-04-27 11:09:33 -0400

    feat.ped-data(Shared): Initial

---
+++ FirstResponseMP.Shared/Objects/PedData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FirstResponseMP.Shared.Objects;
+
+public class PedData(string firstName, string lastName)
+{
+    public string FirstName { get; } = firstName;
+    public string LastName { get; } = lastName;
+    public string FullName => FirstName + " " + LastName;
+    public List<string> Warrants { get; } = []; // TODO?: Refactor into object
+    // TODO?: Gender
+
+
+
+    public void Regenerate() => throw new NotImplementedException();
+    public void RegenerateWarrants() => throw new NotImplementedException();
+}
