34d5a3861cee3b8aa5dfc0bc2f2ce649a307d033
Kilo <37485242+DevKilo1@users.noreply.github.com>
2025-04-27 11:20:45 -0400

    Created SelectRandom list extension.

---
+++ FirstResponseMP.Shared/Extensions/ListExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FirstResponseMP.Shared.Extensions;
+
+public static class ListExtensions
+{
+    private static readonly Random Random = new Random();
+    public static T? SelectRandom<T>(this List<T?> list) => list[Random.Next(Math.Max(0, list.Count - 1))] ?? default;
+}
