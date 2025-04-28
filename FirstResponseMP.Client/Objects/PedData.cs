3c2e84f7ff83b2030ef9d4655e05c6c9a795d3ab
DevKilo1 <luknik2012@gmail.com>
2025-04-28 17:39:06 -0400

    Added Regenerate logic to PedData

--- FirstResponseMP.Client/Objects/PedData.cs
+++ FirstResponseMP.Client/Objects/PedData.cs
@@ -1,22 +1,53 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using CitizenFX.Core;
+using FirstResponseMP.Shared.Extensions;
 using FirstResponseMP.Shared.Interfaces;
+using RandomNameGeneratorLibrary;
 
 namespace FirstResponseMP.Client.Objects;
 // TODO?: Replicate for server
 
-public class PedData(string firstName, string lastName) : IPedData
+public class PedData : IPedData
 {
-    public string FirstName { get; } = firstName;
-    public string LastName { get; } = lastName;
+    private const int WarrantChance = 20;
+
+    private static List<string> PossibleWarrants = [
+        "Bench Warrant"
+    ];
+    
+    public PedData(Gender gender)
+    {
+        Regenerate(gender);
+        RegenerateWarrants();
+    }
+
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
     public string FullName => FirstName + " " + LastName;
-    public List<string> Warrants { get; } = []; // TODO?: Refactor into object
+    public List<string> Warrants { get; private set; } = []; // TODO?: Refactor into object
     // TODO?: Gender
 
 
-    public void Regenerate() => throw new NotImplementedException();
-    public void RegenerateWarrants() => throw new NotImplementedException();
+    public void Regenerate(Gender gender)
+    {
+        FirstName = gender == Gender.Male ? new Random().GenerateRandomMaleFirstName() : new Random().GenerateRandomFemaleFirstName();
+        LastName = new Random().GenerateRandomLastName();
+    }
+
+    public async void RegenerateWarrants()
+    {
+        Warrants.Clear();
+        var rnd = new Random();
+        while (rnd.Next(1, 101) < WarrantChance)
+        {
+            var selectWarrant = PossibleWarrants.SelectRandom();
+            if (!Warrants.Contains(selectWarrant))
+                Warrants.Add(selectWarrant);
+            await BaseScript.Delay(100);
+        }
+    }
 
     public override string ToString()
     {
