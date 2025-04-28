b0cee52f294a1b8f4a031e4d6661d3d9637e0ccb
DevKilo1 <luknik2012@gmail.com>
2025-04-28 17:38:17 -0400

    Updated SelectRandom List extension

--- FirstResponseMP.Shared/Extensions/ListExtensions.cs
+++ FirstResponseMP.Shared/Extensions/ListExtensions.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FirstResponseMP.Shared.Extensions;
 
 public static class ListExtensions
 {
     private static readonly Random Random = new Random();
-    public static T? SelectRandom<T>(this List<T?> list) => list[Random.Next(Math.Max(0, list.Count - 1))] ?? default;
+
+    public static T SelectRandom<T>(this List<T> list)
+    {
+        if (!list.Any())
+            throw new ArgumentException("List must contain at least one element");
+        return list[Random.Next(Math.Max(0, list.Count - 1))];
+    }
 }
